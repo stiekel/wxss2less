203d4f160e01e14b0fcc7f3bb35a21fd11f0467f
Stiekel <histkc@gmail.com>
2017-12-13 10:10:24 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # wxss2less
-rename wxss file to less
+
+`wepy` project must import `less` file, but `weui-wsss` only provide `wxss` files, `wxss2less` can rename all wxss files in one folder to less by one line command.
+
+## Install
+
+```sh
+npm install -g wxss2less
+```
+
+## Usage
+
+Go to wxss style file folder, and run:
+
+```sh
+wxss2less
+```
+
+or you can give the wxss folder as a argument like this:
+
+```sh
+wxss2less src/style
+```
+
+it's done!
---
+++ wxss2less.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+
+'use strict';
+const path = require('path')
+const fs = require('fs')
+const shell = require('shelljs')
+const colors = require('colors')
+
+// get the egret dist path
+let targetPath = process.cwd()
+if (process.argv.length >= 3) {
+  targetPath = process.argv[process.argv.length - 1]
+}
+targetPath = path.resolve(targetPath)
+
+// read all file in a dir
+let readDir = (filepath) => {
+  let files = fs.readdirSync(filepath)
+  let result = []
+  files.map(fn => {
+    let thisPath = path.resolve(filepath, fn)
+    let stats = fs.lstatSync(thisPath)
+    if (stats.isFile()) {
+      if (fn.search('.wxss') === fn.length - 5) {
+        result.push(thisPath)
+      }
+    } else if (stats.isDirectory()) {
+      result = result.concat(readDir(thisPath))
+    }
+  })
+
+  return result
+}
+
+let wxssFiles = readDir(targetPath)
+let successCnt = 0
+// start to rename file
+wxssFiles.map(wf => {
+  let nwf = wf.substr(0, wf.length - 4) + 'less'
+  let r = shell.mv(wf, nwf)
+  if (!r || r.code !== 0) {
+    console.log('[FAILED]'.red, 'rename'.yellow, wf.cyan, 'to', path.basename(nwf).cyan)
+  } else {
+    successCnt++
+    console.log('[SUCCESS]'.green, 'rename'.yellow, wf.cyan, 'to', path.basename(nwf).cyan)
+  }
+})
+console.log('total', String(wxssFiles.length).cyan, '.wxss files,', String(successCnt).green, 'renamed as less')
